97a3b967ebc1f5369992d977f0253a7ac845051d
Sir_G <sirg2003@gmail.com>
2018-01-25 10:27:46 +1100

    test

--- src/ios/GameAnalyticsPlugin.m
+++ src/ios/GameAnalyticsPlugin.m
@@ -14,9 +14,7 @@
     NSString *buildString = [command.arguments objectAtIndex:0];
     [GameAnalytics configureBuild:buildString];
 }
-- (void) configureSdkGameEngineVersion:(CDVInvokedUrlCommand*)command {
-
-}
+- (void) configureSdkGameEngineVersion:(CDVInvokedUrlCommand*)command {}
 - (void) configureGameEngineVersion:(CDVInvokedUrlCommand*)command {
     NSString *gameEngineVersionString = [command.arguments objectAtIndex:0];
     [GameAnalytics configureEngineVersion:gameEngineVersionString];
@@ -27,7 +25,19 @@
 }
 
 - (void) addBusinessEvent:(CDVInvokedUrlCommand*)command {
-
+    NSString *currencyString = [command.arguments objectAtIndex:0];
+    NSInteger amountInteger = [command.arguments objectAtIndex:1];
+    NSString *itemTypeString = [command.arguments objectAtIndex:2];
+    NSString *itemIdString = [command.arguments objectAtIndex:3];
+    NSString *cartTypeString = [command.arguments objectAtIndex:4];
+    NSString *receiptString = nil;
+    
+    [GameAnalytics addBusinessEventWithCurrency:currencyString
+                                         amount:amountInteger
+                                       itemType:itemTypeString
+                                         itemId:itemIdString
+                                       cartType:cartTypeString
+                                        receipt:receiptString];
 }
 - (void) addBusinessEventWithReceipt:(CDVInvokedUrlCommand*)command {
 
